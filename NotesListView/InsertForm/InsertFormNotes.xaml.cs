cd1b00091a0c0d7e67cf8e556d80dadf84f0665e
Anton <fiery2017@yandex.ru>
2024-02-29 19:36:49 +0300

    Initial commit

---
+++ NotesListView/InsertForm/InsertFormNotes.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NotesListView.InsertForm
+{
+    /// <summary>
+    /// Логика взаимодействия для InsertFormNotes.xaml
+    /// </summary>
+    public partial class InsertFormNotes : Window
+    {
+        public InsertFormNotes()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NotesListView/NotesForm/MainNotesForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NotesListView.NotesForm
+{
+    /// <summary>
+    /// Логика взаимодействия для MainNotesForm.xaml
+    /// </summary>
+    public partial class MainNotesForm : Window
+    {
+        public MainNotesForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
